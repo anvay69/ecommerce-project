8a69f5e8295a2168f8a5b7d5847ffae3d20df4fc
Anvay Jain <anvayjain64@gmail.com>
2026-06-02 23:27:29 +0530

    Use axios to fetch data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Header from '../components/Header';
 import './HomePage.css';
 
@@ -8,12 +9,10 @@ import { products } from '../../starting-code/data/products';
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 
 export default function HomePage() {
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      return response.json();
-    }).then((data) => {
-      console.log(data);
-    });
+      console.log(response.data);
+    })
 
   return (
     <>
