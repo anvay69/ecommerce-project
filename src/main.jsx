fee0df1786745ccf0999827c5c5f2a37205f6ae9
Anvay Jain <anvayjain64@gmail.com>
2026-06-04 01:10:14 +0530

    Added axios to console for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios'
+
+// adding axios for debugging purposes
+window.axios = axios;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
